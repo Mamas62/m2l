be02a222917541bce36e45a7015127802aa6a5e0
Mamas62 <malik.lela.62@gmail.com>
2014-10-13 17:33:00 +0200

    modification des grilles boostrap + modification fichier css pour couleur navbar

--- Administrateur/indexadmin.php
+++ Administrateur/indexadmin.php
@@ -1,54 +1,64 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>Accueil Administrateur</title>
+        <title>Accueil Administrateur</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.css" rel="stylesheet">
-    
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+        <!-- Bootstrap core CSS -->
+        <link href="../css/bootstrap.css" rel="stylesheet">
 
-  <body>
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-            <a class="navbar-brand" href="../Login.php">M2L</a>
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="../Login.php">M2L</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="../Administrateur/indexadmin.php">Accueil Administrateur</a></li>
+                        <li><a href="../Administrateur/usagers.php">Utilisateurs</a></li>
+                        <li><a href=../Administrateur/salles.php>Salles</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
         </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="../Administrateur/indexadmin.php">Accueil Administrateur</a></li>
-            <li><a href="../Administrateur/usagers.php">Utilisateurs</a></li>
-            <li><a href=../Administrateur/salles.php>Salles</a></li>
-          </ul><
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
 
-    <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="../img/LogoM2L.jpg" width="200" height="200" alt="Logo M2L">
+                </div>
+                <div class="col-md-9">
+                    <h1 class="text-center" style="margin-bottom: 40px;">Page d'accueil Administrateur</h1>
+                </div>
+                
+                <div class="col-md-9 text-center">
+                    <p class="lead">DECALAGE DE TEXTE SUR LA DROITE.</p>
+                </div>
+            </div>
+            <div class="row">
 
-      <div class="starter-template">
-        <h1> <br> </h1>
-        <h2><center>Page d'accueil Administrateur</center></h2>
-        <h3><left><img src="../img/LogoM2L.jpg" width="200" height="200" alt="Logo M2L"></left></h3> 
-        <p class="lead">CECI EST UN TEST.</p>
-      </div>
+            </div>
+
+        </div>
 
     </div><!-- /.container -->
 
@@ -59,5 +69,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-  </body>
+</body>
 </html>
